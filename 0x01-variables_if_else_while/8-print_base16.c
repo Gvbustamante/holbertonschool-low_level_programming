bffaca26ba1842322388c6b12aca58fd8f7cc48a
Gvbustamante <gvbustamante02@gmail.com>
2020-06-15 11:05:04 -0500

    correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - use for
+ * Description: use putchar to print the alphabet
+ * Return: 0
  */
 int main(void)
 {
-int a = 48;
-while (a < 58)
-{
+int a;
+for (a = '0'; a <= '9' ; a++)
 putchar(a);
-a = (a + 1);
-}
-int b = 97;
-while (b < 103)
-{
-putchar(b);
-b = (b + 1);
-}
+for (a = 'a'; a < 'g' ; a++)
+putchar(a);
 putchar('\n');
 return (0);
 }
