f7bb39a9470889fd418934fb3f1509acef6a31bc
Gvbustamante <65925626+Gvbustamante@users.noreply.github.com>
2020-06-15 20:32:52 -0500

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,8 @@ void print_alphabet_x10(void)
 {
 for (int a = 0; a <= 9 ; a++)
 {
-for (int letter = 'a'; letter <= 'z' ; letter++)
-_putchar(letter);
+for (int alpha = 'a'; alpha <= 'z' ; alpha++)
+_putchar(alpha);
 _putchar('\n');
 }
 return;
