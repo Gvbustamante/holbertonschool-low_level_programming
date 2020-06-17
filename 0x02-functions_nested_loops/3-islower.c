76b7785b0792e464070785387d242d55f6678497
Gvbustamante <gvbustamante02@gmail.com>
2020-06-16 19:02:40 -0500

    function created islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,11 @@
+/**
+ * function that checks for lowercase character.
+ * Description: created of fuction _islower 'c'
+ * Return: one
+ */
 int _islower(int c)
 {
-if (c >= 97 && c <= 122 )
+if (c >= 97 && c <= 122)
 {
 return (1);
 }
