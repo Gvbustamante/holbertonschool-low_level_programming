915ec6db4857dc727f47ac8b10f235ccbb334853
Gvbustamante <gvbustamante02@gmail.com>
2020-06-16 22:06:19 -0500

    value absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @r : parameter choose
+ * Return: r when is positive else *
+ */
+int _abs(int r)
+{
+if (r < 0)
+{
+return (r * -1);
+}
+return (r);
+}
