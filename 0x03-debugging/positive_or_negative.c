922972978f6c99cebef1087596e23db1f8451492
Gvbustamante <gvbustamante02@gmail.com>
2020-06-18 22:30:12 -0500

    positive or negativo

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+}
