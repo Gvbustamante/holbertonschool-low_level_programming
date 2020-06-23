d368a6238240dffc9cd9bd94f2d72891997417b1
Gvbustamante <gvbustamante02@gmail.com>
2020-06-22 21:49:52 -0500

    answer tow

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks for uppercase letters
+ * @c: the character to be checked
+ * Return: 1 if uppercase and, 0 otherwise
+ */
+int mul(int a, int b)
+{
+  return ( a * b);
+
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -9,4 +9,5 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif /* HOLBERTON_H */
