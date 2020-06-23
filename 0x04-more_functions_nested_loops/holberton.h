9b42b7cc4820911bc9cb7261bf39b8a60a967e6a
Gvbustamante <gvbustamante02@gmail.com>
2020-06-22 22:40:44 -0500

    putchar

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,7 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+
 #include <stdio.h>
+int _putchar(char c);
 void print_numbers(void);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
