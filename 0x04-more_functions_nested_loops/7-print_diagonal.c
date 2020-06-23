7ec706587864dca6e1cf9ff6628730a71670af2b
Gvbustamante <gvbustamante02@gmail.com>
2020-06-22 23:37:47 -0500

    git

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints most number followd by a new line
+ * @n : parametre
+ */
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
+for (i = 0; i < n; i++)
+{
+int j;
+for(j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
