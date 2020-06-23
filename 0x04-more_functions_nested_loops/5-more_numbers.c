68c514a9e0dd50ab0e7765dd5853d4f54f36a9e4
Gvbustamante <gvbustamante02@gmail.com>
2020-06-22 22:58:54 -0500

    hg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - prints most number followd by a new line
+ * more_numbers - prints most number followd by a new line
  */
 void more_numbers(void)
 {
@@ -11,13 +11,14 @@ int num = '0';
 int diez = 0;
 for (int j = 0; j < 14; j++)
 {
-if(diez == 1)
+if (diez == 1)
 {
 _putchar('1');
 }
 _putchar(num);
-if(num == '9'){
-num='0';
+if (num == '9')
+{
+num = '0';
 diez = 1;
 }
 num++;
