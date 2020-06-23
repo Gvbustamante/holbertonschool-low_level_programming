3074036d35ec0be7c0f84ce50b27e3f42b9b2a31
Gvbustamante <gvbustamante02@gmail.com>
2020-06-22 22:14:14 -0500

    pilas

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 /**
- * _isdigit - checks for uppercase letters
- * @i: the character to be checked
+ * print_numbers - checks for uppercase letters
  * Return: 1 if uppercase and, 0 otherwise
  */
 void print_numbers(void)
 {
-for (int i=48; i < 58;i++)
+for (int i = 48; i < 58; i++)
 {
 putchar(i);
 }
