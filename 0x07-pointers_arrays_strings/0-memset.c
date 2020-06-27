0465f52c79eecab75baf2c09937f5545e9969aac
Gvbustamante <gvbustamante02@gmail.com>
2020-06-26 23:26:20 -0500

    tarde

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to e filled
+ * @b: char to be copied
+ * @n: numbr of times to be copied
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
